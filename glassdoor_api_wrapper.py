9c9135df900bb960c0c62f3b89dcdbd3f68bfdcd
Thunder Shiviah <thunder@uoregon.edu>
2015-07-29 12:42:12 -0700

    Created working python wrapper for glassdoor api

---
+++ glassdoor_api_wrapper.py
@@ -0,0 +1,33 @@
+# TODO: write a wrapper that checks if running with python 2 and exits.
+
+import json
+import urllib.request as request # urllib.requests requires python3.
+import requests
+import config
+
+""" A small wrapper for Glassdoor's API. See http://stackoverflow.com/questions/30956891/rest-glassdoor-api-requires-user-agent-in-header for some useful info."""
+
+ myIP = '71.238.125.121'
+PARTNER_ID = config.partnerID
+KEY = config.key
+USERAGENT = "Mozilla/5.0"
+MY_IP = json.loads(request.urlopen("http://ip.jsontest.com/").read().decode('utf-8'))['ip'] # From the stackoverflow answer
+url = 'http://api.glassdoor.com/api/api.htm' 
+
+payload = {'v':'1',
+            'format':'json',
+            't.p':PARTNER_ID,
+            't.k':KEY,
+            'userip':MY_IP,
+            'useragent': USERAGENT,
+            'action':'jobs-stats',
+            'returnJobTitles':'true',
+            'returnStates':'true'}
+
+if __name__ == '__main__': 
+    r = requests.get(url, 
+            params=payload,
+            headers =  {'User-Agent': USERAGENT}
+            )
+    print(r.status_code)
+    print(r.headers)
